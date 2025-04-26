9eb224cd1274ccc5977f1b0a6952f7f5a8fb4c86
ArthurViniDev <kenzygamedev@gmail.com>
2025-04-26 15:48:18 -0300

    animation state basic system

--- Assets/Scripts/Units/BaseUnit.cs
+++ Assets/Scripts/Units/BaseUnit.cs
@@ -6,58 +6,70 @@ using UnityEngine.AI;
 public class BaseUnit : MonoBehaviour, ISelectable, IWalkable
 {
     public int health;
-    public float speed;
     public float range;
 
     [SerializeField] private GameObject selectionMark;
     [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Animator animator;
 
-    public string currentAnimation = "Idle";
-
+    private AnimationController animationController;
     void Start()
     {
+        animator = GetComponent<Animator>();
+        animationController = new AnimationController(animator);
         selectionMark = transform.GetChild(0).gameObject;
         agent = GetComponent<NavMeshAgent>();
-        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         bool isMoving = agent.velocity.magnitude > 0.1f;
 
-        if (isMoving && currentAnimation != "isMoving") OnMove();
-        else if (!isMoving && currentAnimation != "isIdle") OnStop();
+        if (isMoving) OnMove();
+        else OnStop();
     }
 
     public void OnMove()
     {
-        Debug.Log("Moving");
-        ChangeAnimation("Walk");
-    }
-    public void OnStop()
-    {
-        ChangeAnimation("Idle");
+        animationController.ChangeAnimation("Walk");
     }
 
-    private void ChangeAnimation(string animationName)
+    public void OnStop()
     {
-        if (currentAnimation == animationName) return;
-        currentAnimation = animationName;
-        animator.CrossFade(animationName, 0.1f);
+        animationController.ChangeAnimation("Idle");
     }
 
     public void OnSelect()
     {
         selectionMark.SetActive(true);
     }
+    
     public void OnDeselect()
     {
         selectionMark.SetActive(false);
     }
+
     public void Move(Vector3 targetPosition)
     {
         if (targetPosition == null) return;
         agent.SetDestination(targetPosition);
+    }
+}
+
+public class AnimationController
+{
+    private Animator animator;
+    private string currentAnimation;
+
+    public AnimationController(Animator animator)
+    {
+        this.animator = animator;
+    }
+
+    public void ChangeAnimation(string animationName)
+    {
+        if (currentAnimation == animationName) return;
+        currentAnimation = animationName;
+        animator.CrossFade(animationName, 0.1f);
     }
 }
