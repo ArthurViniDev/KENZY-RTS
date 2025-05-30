3495b20dfc9dcc3fa97208d4e5be06bcaeeb1e04
ArthurViniDev <kenzygamedev@gmail.com>
2025-05-30 13:02:59 -0300

    instantiate builds

--- Assets/Scripts/Player/PlacementSystem.cs
+++ Assets/Scripts/Player/PlacementSystem.cs
@@ -1,13 +1,18 @@
+using System;
 using UnityEngine;
 
 public class PlacementSystem : MonoBehaviour
 {
-    [SerializeField] private GameObject mouseIndicator, cellIndicator, buildingSystem;
+    [Header("References")]
     [SerializeField] private GridManager gridManager;
+    [SerializeField] private GameObject mouseIndicator, cellIndicator, buildingSystem;
+    [SerializeField] private Vector3 buildOffset;
     [SerializeField] private Grid grid;
 
     private BuildSelectionUI buildSelectionUI;
     private Build build;
+
+    private bool hasSelection;
 
     private void Awake()
     {
@@ -24,14 +29,25 @@ public class PlacementSystem : MonoBehaviour
 
         mouseIndicator.transform.position = mousePosition;
         cellIndicator.transform.position = grid.CellToWorld(gridPosition);
+
+        if(Input.GetMouseButtonDown(1) && hasSelection)
+        {
+            BuildConstruction();
+        }
+    }
+
+    private void BuildConstruction()
+    {
+        
+        Instantiate(buildSelectionUI.selectedBuildingPrefab, cellIndicator.transform.position + buildOffset, Quaternion.Euler(-90f, 0f, 0f));
     }
 
     private void UpdateIndicators()
     {
-        bool hasSelection = buildSelectionUI.selectedBuildingPrefab != null;
+        hasSelection = buildSelectionUI.selectedBuildingPrefab != null;
         build.CursorIndicatorParent.SetActive(hasSelection);
+        mouseIndicator.SetActive(hasSelection);
 
-        if (buildSelectionUI.selectedBuildingPrefab != null)
-            buildSelectionUI.selectedBuildingPrefab.SetActive(true);
+        if (buildSelectionUI.selectedBuildingPrefab != null) buildSelectionUI.selectedBuildingPrefab.SetActive(true);
     }
 }
